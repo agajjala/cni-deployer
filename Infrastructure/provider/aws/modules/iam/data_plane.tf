3d80435b8f754f5522595026fc6b667d65fec444
Abhiram Gajjala <agajjala@salesforce.com>
2020-05-06 15:15:13 -0700

    CNI LAMBDA Metrics and New IAM Permissions for DP-Node-Group (#72)
    * Add LAMBDA Metrics

--- Infrastructure/provider/aws/modules/iam/data_plane.tf
+++ Infrastructure/provider/aws/modules/iam/data_plane.tf
@@ -47,6 +47,16 @@ resource aws_iam_role_policy_attachment data_plane_node_group_cloudwatch_agent_a
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
+resource aws_iam_role_policy_attachment data_plane_node_group_cloudwatch_read_access {
+  role       = aws_iam_role.data_plane_node_group.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
+}
+
+resource aws_iam_role_policy_attachment data_plane_node_group_secrets_manager_access {
+  role       = aws_iam_role.data_plane_node_group.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+}
+
 resource aws_iam_role_policy_attachment data_plane_node_group_autoscaling {
   role       = aws_iam_role.data_plane_node_group.name
   policy_arn = aws_iam_policy.data_plane_node_group_autoscaling.arn
