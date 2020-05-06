7fd2208588baf6ac2cca329ae7c08dbdffbbeb48
Abhiram Gajjala <agajjala@salesforce.com>
2020-05-05 22:16:39 -0700

    update dp-node-group permissions

--- Infrastructure/provider/aws/modules/iam/data_plane.tf
+++ Infrastructure/provider/aws/modules/iam/data_plane.tf
@@ -42,12 +42,12 @@ resource aws_iam_role data_plane_node_group {
   })
 }
 
-resource aws_iam_role_policy_attachment data_plane_node_group_cloudwatch_read_access {
+resource aws_iam_role_policy_attachment data_plane_node_group_cloudwatch_agent_access {
   role       = aws_iam_role.data_plane_node_group.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
-resource aws_iam_role_policy_attachment data_plane_node_group_cloudwatch_agent_access {
+resource aws_iam_role_policy_attachment data_plane_node_group_cloudwatch_read_access {
   role       = aws_iam_role.data_plane_node_group.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
 }
