aba8a79f7cf73d49c0462aff3c36945d79fbee3c
Abhiram Gajjala <agajjala@salesforce.com>
2020-05-07 19:22:34 -0700

    Enable Stage1 and modify ECR_PUSH Script (#75)
    * Enable Stage1 with the latest tags

--- push_new_image_version.sh
+++ push_new_image_version.sh
@@ -3,26 +3,28 @@
 set -eou pipefail
 
 # Usage:
-# ./push_new_image_version.sh <IMAGE_NAME> <NEW_IMAGE_VERSION> <REGION_1> <REGION_2> ..
+# ./push_new_image_version.sh <STRATA_IMAGE_VERSION> <REGION_1> <REGION_2> ..
 #
 # Example:
-# ./push_new_image_version.sh cni-metrics-exporter 1 us-west-2 us-east-1
+# ./push_new_image_version.sh 280 us-west-2 us-east-1
 
-IMAGE_NAME=$1
-NEW_IMAGE_VERSION=$2
-REGIONS=("${@:3}")
+STRATA_IMAGE_VERSION=$1
+REGIONS=("${@:2}")
 ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
 
-docker pull dva-registry.internal.salesforce.com/dva/sdn/"$IMAGE_NAME":latest
-
-IMAGE_ID=$(docker images --format='{{json .}}' | grep "$IMAGE_NAME" | grep latest | jq -r '.ID')
-
-for region in "${REGIONS[@]}"
+CNI_IMAGES=( "cni-metrics-exporter" "cni-monitoring" "cni-nginx-proxy-inbound" "cni-nginx-proxy-outbound" )
+docker login dva-registry.internal.salesforce.com
+for imageName in "${CNI_IMAGES[@]}"
 do
-  ECR_IMAGE_NAME="$ACCOUNT_ID.dkr.ecr.$region.amazonaws.com/$IMAGE_NAME"
-  ECR_IMAGE_NAME_TAG="$ECR_IMAGE_NAME:$NEW_IMAGE_VERSION"
+  docker pull dva-registry.internal.salesforce.com/dva/sdn/"$imageName":"$STRATA_IMAGE_VERSION"
+  printf "Pushing %s to ECR...\n" "${imageName}"
+  for region in "${REGIONS[@]}"
+  do
+    ECR_IMAGE_NAME="$ACCOUNT_ID.dkr.ecr.$region.amazonaws.com/$imageName"
+    ECR_IMAGE_NAME_TAG="$ECR_IMAGE_NAME:$STRATA_IMAGE_VERSION"
 
-  aws ecr get-login-password --region "$region" | docker login --username AWS --password-stdin "$ECR_IMAGE_NAME"
-  docker tag "$IMAGE_ID" "$ECR_IMAGE_NAME_TAG"
-  docker push "$ECR_IMAGE_NAME_TAG"
+    $(aws ecr get-login --no-include-email --region "$region")
+    docker tag dva-registry.internal.salesforce.com/dva/sdn/"$imageName":"$STRATA_IMAGE_VERSION" "$ECR_IMAGE_NAME_TAG"
+    docker push "$ECR_IMAGE_NAME_TAG"
+  done
 done
