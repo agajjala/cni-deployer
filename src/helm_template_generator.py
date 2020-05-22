cb6282a7eece94ef4525ab815580c6599bbbe87c
Abhiram Gajjala <agajjala@salesforce.com>
2020-05-22 09:42:10 -0700

    Update image

--- src/helm_template_generator.py
+++ src/helm_template_generator.py
@@ -42,6 +42,7 @@ class ManifestProcessor:
             [
                 "helm",
                 "template",
+                manifest_data["deployment_id"],
                 INBOUND_HELM_TEMPLATE_PATH,
                 "--values",
                 manifest_file,
@@ -62,6 +63,7 @@ class ManifestProcessor:
             [
                 "helm",
                 "template",
+                manifest_data["deployment_id"],
                 OUTBOUND_HELM_TEMPLATE_PATH,
                 "--values",
                 manifest_file,
@@ -104,7 +106,6 @@ class ManifestProcessor:
     def process(self):
         self.process_manifest_file(self.manifest_path)
 
-
 def get_fixed_arguments(args):
     """
     return the fixed arguments as a dictionary
@@ -113,9 +114,10 @@ def get_fixed_arguments(args):
     """
     return vars(args)
 
-
 def main():
-    parser = argparse.ArgumentParser(description="This program generates EKS Helm Templates for manifests.")
+    parser = argparse.ArgumentParser(
+        description="This program generates EKS Helm Templates for manifests."
+    )
     parser.add_argument("--manifest_path", help="path to the manifest file describing the deployment")
     args = parser.parse_args()
     deploy_args = get_fixed_arguments(args)
