14cede371b089c9ac95b62e4859dca8d7bdeb065
Abhiram Gajjala <agajjala@salesforce.com>
2020-06-12 23:00:14 -0700

    CHeck for Key instead of value

--- src/eks_setup_helper.py
+++ src/eks_setup_helper.py
@@ -403,7 +403,7 @@ def outbound_eks_nlb_setup(deploy_stage, manifest_data):
                 )
 
                 # Add CNAME Records for OUTBOUND EKS NLB NAMES IN SFDCSB.NET Zone
-                if not manifest_data["enable_sitebridge"]:
+                if "enable_sitebridge" in manifest_data:
                     print("********** OUTBOUND EKS NLB DNS SETUP ***********")
                     dns_name = "core-{}.{}.aws.{}.cni{}.sfdcsb.net".format(
                         vpc_suffix, manifest_data["env_name"], manifest_data["region"], manifest_data["env_name"]
@@ -439,12 +439,12 @@ def outbound_eks_nlb_setup(deploy_stage, manifest_data):
                 infra_vpc_info["security_group_ids"] = sg_list
                 infra_vpc_info["status"] = "inService"
                 infra_vpc_info["total_capacity"] = 500
-                if not manifest_data["enable_sitebridge"]:
-                    infra_vpc_info["proxy_url"] = "https://{}:443".format(nlb_name)
-                else:
+                if "enable_sitebridge" in manifest_data:
                     infra_vpc_info["proxy_url"] = "https://core-{}.{}.aws.{}.cni{}.sfdcsb.net:443".format(
                         vpc_suffix, manifest_data["env_name"], manifest_data["region"], manifest_data["env_name"]
                     )
+                else:
+                    infra_vpc_info["proxy_url"] = "https://{}:443".format(nlb_name)
                 outbound_infra_vpcs_info.append(infra_vpc_info)
             print("******** OUTBOUND INFRA VPC'S DDB SETUP *********")
             outbound_ddb_item = {"id": {"S": "infra_vpcs"}, "Payload": {"S": json.dumps(outbound_infra_vpcs_info)}}
